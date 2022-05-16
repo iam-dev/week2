e717f549ffc0f3b6d182ca1e6e1ad2467c3ee897
iam-dev <elky.bachtiar@duo.nl>
2022-05-16 20:30:51 +0200

    Week2 assigments

---
+++ Part1/circuits/poseidonHashSwitcher.circom
@@ -0,0 +1,14 @@
+include "./Poseidon/poseidonHashT3.circom";
+
+template PoseidonHashSwitcher() {
+    signal input left;
+    signal input right;
+
+    signal output hash;
+
+    component hasher = PoseidonHashT3();
+    left ==> hasher.inputs[0];
+    right ==> hasher.inputs[1];
+
+    hash <== hasher.out;
+}
--- Part1/test/merkle-test.js
+++ Part1/test/merkle-test.js
@@ -44,12 +44,33 @@ describe("MerkleTree", function () {
         await merkleTree.deployed();
     });
 
+    it("Should return 15 leaves", async function () {
+        expect(await merkleTree.hashLength()).to.equal(15);
+    });
+
+    it("Should return 8 blank leaves", async function () {
+        for(let i = 0; i < 8; i++) {
+            expect(await merkleTree.hashByIndex(i)).to.equal(0);
+        }
+    });
+
+    it("Should return root leave", async function () {
+        const root = await merkleTree.root();
+        console.log("root", root);
+        const total = await merkleTree.hashLength();
+        console.log("total", total);
+        expect(await merkleTree.hashByIndex(total-1)).to.equal(root);
+    });
+
     it("Insert two new leaves and verify the first leaf in an inclusion proof", async function () {
+        const oldRoot = await merkleTree.root();
         await merkleTree.insertLeaf(1);
         await merkleTree.insertLeaf(2);
+        const newRoot = await merkleTree.root();
+        expect(newRoot).to.not.equal(oldRoot);
 
-        const node9 = (await merkleTree.hashes(9)).toString();
-        const node13 = (await merkleTree.hashes(13)).toString();
+        const node9 = (await merkleTree.hashByIndex(9)).toString();
+        const node13 = (await merkleTree.hashByIndex(13)).toString();
 
         const Input = {
             "leaf": "1",
@@ -72,5 +93,33 @@ describe("MerkleTree", function () {
         expect(await merkleTree.verify(a, b, c, input)).to.be.true;
 
         // [bonus] verify the second leaf with the inclusion proof
+    });
+    it("Insert two new leaves and verify the second leaf in an inclusion proof", async function () {
+        // [bonus] verify the second leaf with the inclusion proof
+        await merkleTree.insertLeaf(1);
+        await merkleTree.insertLeaf(2);
+
+        const node9 = (await merkleTree.hashByIndex(9)).toString();
+        const node13 = (await merkleTree.hashByIndex(13)).toString();
+
+        const Input = {
+            "leaf": "2",
+            "path_elements": ["1", node9, node13],
+            "path_index": ["1", "0", "0"]
+        }
+        const { proof, publicSignals } = await groth16.fullProve(Input, "circuits/circuit_js/circuit.wasm","circuits/circuit_final.zkey");
+
+        const editedPublicSignals = unstringifyBigInts(publicSignals);
+        const editedProof = unstringifyBigInts(proof);
+        const calldata = await groth16.exportSolidityCallData(editedProof, editedPublicSignals);
+    
+        const argv = calldata.replace(/["[\]\s]/g, "").split(',').map(x => BigInt(x).toString());
+    
+        const a = [argv[0], argv[1]];
+        const b = [[argv[2], argv[3]], [argv[4], argv[5]]];
+        const c = [argv[6], argv[7]];
+        const input = argv.slice(8);
+
+        expect(await merkleTree.verify(a, b, c, input)).to.be.true;
     });
 });
